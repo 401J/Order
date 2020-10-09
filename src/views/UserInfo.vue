3d56313efdf5fb66f86101239c532bc0d7df4f56
Mr_LiR <Mr_LiR@163.com>
2020-10-09 14:58:19 +0800

    个人中心

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -4,59 +4,40 @@
     <div class="container-fluid all">
       <div class="row">
         <!-- 左侧边栏 -->
-        <div class="col-3">
-          <!-- 上 -->
-          <div class="row bg-secondary">
-            <div id="image">
-              
-              <img
-                width="80px"
-                height="80px"
-                src="../assets/userInfo.jpg"
-                class="rounded-circle"
-                alt="Cinque Terre"
-              />
-            </div>
-            <router-link to="/userInfo/login">
-              <button type="button" class="btn btn-success my-3">登录</button>
+        <div class="col-3 bg-secondary px-0">
+          <!-- 头像 -->
+          <img
+            id="user-img" 
+            src="../assets/userInfo.jpg" 
+            class="rounded-circle">
+          <!-- 登录注册按钮 -->
+          <div class="row img-user">
+            <router-link 
+              class="img-user"
+              to="/userInfo/login">
+              <button type="button" class="btn btn-success">登录</button>
             </router-link>
             <router-link to="/userInfo/register">
-              <button type="button" class="btn btn-danger register">
-                注册
-              </button>
-            </router-link>
+              <button type="button" class="btn btn-danger">注册</button>
+            </router-link>            
           </div>
-          <!-- 中 -->
-          <div class="row"></div>
-          <!-- 下 -->
-          <div class="row">
-            <div
-              v-for="(item, index) in list"
-              :key="index"
-              id="div-list"
-              class="col-6"
-            >
-              <a
-                class="a-list"
-                style="line-height: 100px; text-align: center"
-                href=""
-                >{{ item }}</a
-              >
-            </div>
-            <div class="col-6 bg-dark">
-              <router-link to="">
-                <button type="button" class="btn btn-dark">自主结账</button>
-              </router-link>
-            </div>
-            <div class="col-6 bg-danger">
-              <router-link to="">
-                <button type="button" class="btn btn-danger">积分兑换</button>
-              </router-link>
+
+          <hr width="360px">
+          <!-- 功能菜单 -->
+          <div class="row userlist">
+            <div 
+              class="col-6 px-0 "
+              v-for="(item, index) in list" :key="index">
+              <button 
+                type="button" 
+                class="btn btn-dark text-light">{{item}}</button>
             </div>
           </div>
+
+          
         </div>
         <!-- 右侧边栏 -->
-        <div class="col-9">
+        <div class="col-9 bg-light">
           <h3 id="welcome" style="height: 80px">欢迎光临</h3>
           <router-view></router-view>
         </div>
@@ -78,7 +59,7 @@ export default {
         "我点的菜",
         "历史消费",
         "视频监控",
-        "美甲拍号",
+        "美甲排号",
         "发票管理",
         "我的卡卷",
       ],
@@ -88,59 +69,48 @@ export default {
 </script>
 
 <style>
-.btn.btn-danger.register {
-  position: relative;
-  left: 20px;
-  top: 165px;
-}
-.btn.btn-success.my-3 {
-  position: relative;
-  left: -45px;
-  top: 150px;
+/* 用户头像样式 */
+#user-img{
+  width:80px;
+  margin: 50px 0px 0px 140px;
 }
 
-#welcome {
-  float: left;
-}
-.rounded-circle {
-  position: relative;
-  left: 90px;
-  top: 50px;
-}
-/* 左侧上方 */
-.row.bg-secondary {
-  height: 256px;
-  padding-left: 50px;
-  background-size: cover;
-  background-image: url(../assets/西餐2.jpg);
-  background-color: rgba(125, 125, 125, 0.5);
-  /*加入背景透明，使页面不那么唐突*/
-  /* background-size: cover; */
-  /* filter: blur(1px); */
-}
-#image {
-}
-/* 左侧中间 */
-.row.bg-dark {
-  padding-left: 100px;
-  padding-top: 40px;
-  border: 1px soli white;
-}
-#div-list {
-  background-color: #8e8e8e;
-  border: 1px solid white;
-}
-.col-9 {
-  background: url(../assets/西餐.jpg);
-  height: 600px;
+/* 左侧边栏样式 */
+.col-3.bg-secondary.px-0{
+  border-radius:5px;
+  height: 600px;  
 }
 
-.a-list {
-  color: white;
+/* 右侧边栏样式 */
+.col-9.bg-light{
+  /* background:url(../assets/西餐.jpg) */
 }
-#footer {
+
+/* 设置按钮样式 */
+.row.img-user{
+  padding: 30px 0px 0px 120px;
+}
+.img-user{
+  padding-right: 40px;
+}
+
+/* 功能菜单样式 */
+.row.userlist{
   width: 100%;
+  margin: 0;
+}
+.btn.btn-dark.text-light{
+  width: 100%;
+  height: 80px;
+  background-color: #6c757d;
+  border: solid 1px #6c757d;
+}
+.btn.btn-dark.text-light:hover{
+  background-color:rgb(75,75,75);
+}
+/* 底部 */
+#footer{
   height: 50px;
-  background-color: #4b4b4b;
+  background-color: rgb(75,75,75);
 }
 </style>
