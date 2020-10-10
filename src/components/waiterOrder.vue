eae09369c9c157ae51a8322a7ff2fdbe40a705a3
18684926498 <553975380@qq.com>
2020-10-10 12:57:08 +0800

    danger

---
+++ src/components/waiterOrder.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <WaiterNav></WaiterNav>
+        <div class="card w-25 m-auto">
+          <div class="card-body">
+            <h4 class="card-title">订单详情</h4>
+            <ul id="u1">
+                <!-- 用vfor那边下单这里创建一个 -->
+                <li>第一单<router-link to="/Settle" class="float-right">查看详情</router-link></li>
+            </ul>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import WaiterNav from '../components/waiterNav'
+export default {
+    components:{
+        WaiterNav
+    }
+}
+</script>
+
+<style lang="css">
+    #u1{
+        padding: 0;
+    }
+    #u1 li{
+        list-style: none;
+    }
+</style>
--- src/views/Bill.vue
+++ src/views/Bill.vue
@@ -1,7 +1,25 @@
 <template>
     <div>
         <WaiterNav></WaiterNav>
-        账单
+        <div class="card w-50 m-auto">
+          <div class="card-body">
+            <!-- 用户名传参 -->
+            <router-link class="btn btn-light pt-0 float-right" to="/orders">返回</router-link>
+            <h4 class="card-title">客户：{{username}}</h4>
+            <!-- 时间 -->
+            <p class="card-text">时间：{{time}}</p>
+            <h4>菜品：</h4>
+            <!-- 循环菜单 -->
+            <ul>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+            <div class="row"></div>
+            <!-- 总额 -->
+            <h4>总消费：{{money}}</h4>
+            <h4 class="mt-3">谢谢惠顾，欢迎下次光临！</h4>
+          </div>
+        </div>
     </div>
 </template>
 
@@ -10,10 +28,15 @@ import WaiterNav from '../components/waiterNav'
 export default {
     components:{
         WaiterNav
-    }
+    },
+   data () {
+       return {
+           time: new Date().toLocaleString()
+       }
+   }
 }
 </script>
 
-<style lang="">
+<style lang="css">
     
 </style>
